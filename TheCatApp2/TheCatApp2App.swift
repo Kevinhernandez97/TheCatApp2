8c5c36b5dd386441e2460953be4961d0c8a45b7c
Kevin Hernandez <kmhs-18@hotmail.com>
2023-06-22 14:36:41 -0300

    Initial Commit

---
+++ TheCatApp2/TheCatApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  TheCatApp2App.swift
+//  TheCatApp2
+//
+//  Created by Kevin Hernandez on 22/06/23.
+//
+
+import SwiftUI
+
+@main
+struct TheCatApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
